902ca1e520d361d23d6321280ce38b8ec4d05783
Sunil Islam <developer@sunildev.uk>
2020-10-01 17:47:12 +0000

    Add input review & slight modifications to drone

---
+++ basics/input/review.py
@@ -0,0 +1,5 @@
+def run(name, age, height, weight, health, shield):
+  print(f'Checking database for {name}...')
+  print(f'Hello {name}, One moment I am still looking up your details...')
+  print(f'I have found your details...')
+  print(f'Name: {name}\nAge: {age}\nHeight: {height}\nWeight: {weight}\nShield Levels: {shield}')
