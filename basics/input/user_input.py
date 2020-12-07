0f731976279afa787f7ddb8dc96b6cb853724031
Sunil Islam <developer@sunildev.uk>
2020-12-07 19:31:21 +0000

    Added code to read user input

--- basics/input/user_input.py
+++ basics/input/user_input.py
@@ -1,6 +1,10 @@
 def run():
+  import os
+  debug = os.getenv('debug')
   # Ask user to enter their name
   print("What is your name human?")
-  if ()
-  name = input()
+  if (debug == 'true'):
+    name = "drone"
+  else:
+    name = input()
   print("It is nice to meet you human", name)
--- main.py
+++ main.py
@@ -7,10 +7,13 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
-import os
 
-print(os.getenv('path'))
 print(f'{OKGREEN}Welcome to com411 code runner.{ENDC}\n')
 print(f'{OKGREEN}Running Part A code.{ENDC}')
 from a import execute
+execute()
+
+print()
+print(f'{OKGREEN}Running Part B code.{ENDC}')
+from b import execute
 execute()
